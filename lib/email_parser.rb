4da38a3d305b5d9712ae8c997aeed5fdb8244fdc
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-18 13:11:39 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,9 +2,9 @@ class EmailAddressParser
   attr_accessor :emails_array
   @emails_array = []
   def initialize(emails)
-    p emails
+
     email_separator =  emails.split(/[\s,]/) 
-    p email_separator
+  
     email_separator.each{|el| @emails_array << el if el != ""}
     p @emails_array
   end
